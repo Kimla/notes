0b1e285e1f18f6a75669be6fe8a956a22eefd63a
Kim Larsson <kimla89@gmail.com>
2017-10-08 22:43:29 +0200

    In the slugify function added support to transform åäö into aao

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,5 +1,8 @@
 export function slugify(text) {
     return text.toString().toLowerCase()
+        .replace(/å/g , "a")            // Replace å with a
+        .replace(/ä/g , "a")            // Replace ä with a
+        .replace(/ö/g , "o")            // Replace ö with o
         .replace(/\s+/g, '-')           // Replace spaces with -
         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
